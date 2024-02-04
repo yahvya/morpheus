8604af841cf6ceae1d265314fb10ef461e8ce230
Bathily Yahaya <developpeur.yb@gmail.com>
2024-02-04 12:38:20 +0100

    suppression du test requirements.txt mis à jour

--- api/api.py
+++ api/api.py
@@ -2,13 +2,7 @@
 # @brief point d'entrée de l'api du projet Morpheus
 # @author Vitalli https://github.com/siegward-from
 
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
 
-from treatment.detection.detector import DetectorArg
 
 app = FastAPI()
-
-
-@app.post("/test")
-async def test(file: UploadFile = File(...)):
-    return {"result": DetectorArg.are_videos(file)}
