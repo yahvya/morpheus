915d7328a53fd7402131b3cb9117a5c33ece6ddf
celia336 <celialoucif03@gmail.com>
2024-02-04 21:39:54 +0100

    add function of Button App

--- morpheus/lib/components/AppButton.dart
+++ morpheus/lib/components/AppButton.dart
@@ -2,8 +2,13 @@ import 'package:flutter/cupertino.dart';
 
 /// boutton de l'application
 class AppButton extends StatelessWidget{
+  final VoidCallback onPressed;
+
   @override
   Widget build(BuildContext context){
-    return Text("");
+    return TextButton(
+      onPressed: onPressed,
+      child: Text("Lancer L'application"),
+    );
   }
 }
