5528c4be5744c783e92e5bd17a2bb041c12ac800
celia336 <celialoucif03@gmail.com>
2024-02-06 17:28:53 +0100

    update

--- morpheus/lib/components/AppButton.dart
+++ morpheus/lib/components/AppButton.dart
@@ -1,82 +1,34 @@
 
 import 'package:flutter/cupertino.dart';
 
-/// boutton de l'application
-class AppButton extends StatelessWidget{
+class AppButton extends StatelessWidget {
+  final String text;
   final VoidCallback onPressed;
-
-  const AppButton({Key? key, required this.onPressed}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context){
-    return Container(
-      margin: EdgeInsets.fromLTRB(62, 628, 0, 0),
-      width: 236,
-      height: 52,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        child: Text(
-          "Lancer L'application",
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.white,
-          ),
-        ),
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-        ),
-      ),  
-    ),
-  }
+  final Color color;
+  final Color textcolor;
+ 
+  AppButton(( 
+    text:"Increment",
+    onPressed:_incrementCounter,
+    color: Colors.green,
+    textcolor: Colors.white,
+  ), // AppButton
+      required this. text,
+       required this. onPressed,
+       this.color = Colors.blue,
+       this.textcolor = Colors.white,
+     
+      );
+   @override
+    Widget Build(BuildContext context  ){
+      return ElevatedButton( 
+        style: ElevatedButton.styleFrom( primary:color ),
+        onPressed :onPressed,
+        child:Text(
+           test ,  
+           style:TextStyle(color:textcolor ),
+       
+      ),
+      ); 
+    }
 }
-
-//######################################################################################################
-//################## pour lancer l'application veuillez passer ce bout de code pour lancer la fonction #
-// AppButton(                                                                                          #
-//   onPressed: () {                                                                                   #
-//     // Comportement à exécuter lors du clic sur le bouton                                           #
-//     print("L'application  est lancée !");                                                           #
-//   },                                                                                                #
-// )                                                                                                   #
-//######################################################################################################
-
-//#####################################################################################################################################################
-
-
-class LaunchTutorielButton extends StatelessWidget{
-  final VoidCallback onPressed;
-
-  const LaunchTutorielButton({Key? key, required this.onPressed}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context){
-    return Container(
-      margin: EdgeInsets.fromLTRB(29, 447, 0, 0),
-      width: 289,
-      height: 52,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        child: Text(
-          "Lancer le tutoriel (icn Aws)",
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.white,
-          ),
-        ),
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-        ),
-      ),  
-    ),
-  }
-}
-
-//######################################################################################################
-//################## pour lancer le tutoriel veuillez passer ce bout de code pour lancer la fonction   #
-// LaunchTutorielButton(                                                                               #
-//   onPressed: () {                                                                                   #
-//     // Comportement à exécuter lors du clic sur le bouton                                           #
-//     print("Le tutoriel  est lancé!");                                                               #
-//   },                                                                                                #
-// )                                                                                                   #
-//######################################################################################################
