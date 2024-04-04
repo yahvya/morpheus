ec168d3c8a824361a6b41acd48b6f66ef82f77c3
siegward-from <ntsn44@gmail.com>
2024-04-04 14:24:59 +0200

    Fix signature verification function

--- api/api.py
+++ api/api.py
@@ -2,25 +2,31 @@
 # @brief point d'entrée de l'api du projet Morpheus
 # @author Vitalli https://github.com/siegward-from
 
-from fastapi import FastAPI, UploadFile, File, Request
+from fastapi import FastAPI, UploadFile, File, Request, Form, Header
 
-from api.utils import check_signature
+from api.utils import check_signature, save_upload_file, get_secret_key, generate_signature, save_files
 from treatment.detection.detector import Detector, DetectorArg
 from treatment.exceptions.custom_exception import CustomException
+import os
+
 
 app = FastAPI()
 
 
 @app.post("/video")
 async def treat_video(
-        request: Request,
+        signature: str = Header(...),
         front_video: UploadFile = File(...),
         front_head_move_video: UploadFile = File(...),
         profile_head_up_video: UploadFile = File(...),
         profile_head_down_video: UploadFile = File()
 ):
-    # check_signature(request)
+
     try:
+        files = [front_video, front_head_move_video, profile_head_up_video, profile_head_down_video]
+        video_paths = await save_files(files)
+        check_signature(video_paths, signature)
+
         await Detector.extract_datas(DetectorArg(
             front_video,
             front_head_move_video,
--- api/utils.py
+++ api/utils.py
@@ -1,16 +1,56 @@
 ##
 # @brief point d'entrée de les utils du projet Morpheus
 # @author Vitalli https://github.com/siegward-from
+
 import hmac
 import hashlib
 import base64
 import time
-
 from fastapi import Request, HTTPException
 import secrets
+import os
 
 
-def generate_signature(data, secret_key):
+def generate_signature(key: bytes, files) -> str:
+    hmac_sha256 = hmac.new(key.encode(), digestmod=hashlib.sha256)
+    for file_path in files:
+        with open(file_path, 'rb') as f:
+            while chunk := f.read(4096):
+                hmac_sha256.update(chunk)
+    return hmac_sha256.hexdigest()
+
+
+async def save_upload_file(directory: str, file):
+    file_location = f"{directory}/{file.filename}"
+    with open(file_location, "wb") as buffer:
+        data = await file.read()
+        buffer.write(data)
+    return file_location
+
+
+async def save_files(files):
+    video_dir = '.\\videos'
+    if not os.path.exists(video_dir):
+        os.makedirs(video_dir)
+
+    video_paths = []
+    for video in files:
+        path = await save_upload_file(video_dir, video)
+        video_paths.append(path)
+
+    return video_paths
+
+
+def check_signature(data, signature):
+    secret_key = 'c27f9aad7c97689dffe026a2482bb3878dffbe78ae0e79e90638c72fcc545227'
+    signature_generated = generate_signature(secret_key, data)
+    if signature != signature_generated:
+        raise HTTPException(status_code=400, detail='signature is missing')
+    else:
+        print('Signature verification passed')
+
+
+def generate_base_signature(data, secret_key):
     message = data.encode('utf-8')
     secret = secret_key.encode('utf-8')
 
@@ -20,7 +60,7 @@ def generate_signature(data, secret_key):
     return signature_base64
 
 
-def check_signature(request: Request):
+def check_base_signature(request: Request):
     try:
         signature = request.headers['signature']
     except KeyError:
@@ -31,8 +71,7 @@ def check_signature(request: Request):
     except KeyError:
         raise HTTPException(status_code=400, detail='data is missing')
 
-    with open('key', 'r') as f:
-        key = f.read()
+    key = get_secret_key()
 
     if not signature or not data or not key:
         raise HTTPException(status_code=400, detail='Bad request')
@@ -43,12 +82,6 @@ def check_signature(request: Request):
 
     if not hmac.compare_digest(signature, signature_generated):
         raise HTTPException(status_code=401, detail="Invalid signature")
-
-
-def generate_secret_key(length=32):
-    with open('key', 'w') as f:
-        key = secrets.token_hex(length)
-        f.write(key)
 
 
 def check_signature_with_timestamp(request: Request, tolerance = 300):
@@ -80,3 +113,13 @@ def check_signature_with_timestamp(request: Request, tolerance = 300):
     return False
 
 
+def generate_secret_key(length=32):
+    with open('key', 'w') as f:
+        key = secrets.token_hex(length)
+        f.write(key)
+
+
+def get_secret_key():
+    with open('key', 'r') as f:
+        key = f.read()
+    return key
